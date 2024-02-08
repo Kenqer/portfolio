5faf74b034a386bfb7be6e592b5d2a9b0eb007b2
kenqer <javierkennedi09@gmail.com>
2024-02-08 14:55:12 +0700

    fix li

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,27 +14,29 @@ const Navbar = () => {
                         <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white
                                     md:z-auto z-[99] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all 
                                     duration-500 ease-in font-[jawa] md:flex-row md:gap-20 rounded-br-xl border-b-8 border-teal-700 md:border-none shadow-md md:shadow-none ${open ? 'top-[100px]' : 'left-[-1000px] top-[100px]'}`}>
-                            <Link to="/">
-                                <li className="flex flex-col group">
+                            <li className="flex flex-col group">
+                                <Link to="/">
                                     <span className="text-xl font-semibold hover:text-teal-600 cursor-pointer group-hover:scale-105 duration-200 z-[2]">Home</span>
                                     <span className="w-[300px] md:w-full border-b-[5px] rounded-b-md border-teal-600 scale-0 group-hover:scale-110 duration-200">
                                     </span>
-                                </li>
-                            </Link>
-                            <Link to="/about">
-                                <li className="flex flex-col group">
+                                </Link>
+                            </li>
+
+                            <li className="flex flex-col group">
+                                <Link to="/about">
                                     <span className="text-xl font-semibold hover:text-teal-600 cursor-pointer group-hover:scale-105 duration-200 z-[2]">About</span>
                                     <span className="w-[300px] md:w-full border-b-[5px] rounded-b-md border-teal-600 scale-0 group-hover:scale-110 duration-200">
                                     </span>
-                                </li>
-                            </Link>
-                            <Link to="/contact">
-                                <li className="flex flex-col group">
+                                </Link>
+                            </li>
+
+                            <li className="flex flex-col group">
+                                <Link to="/contact">
                                     <span className="text-xl font-semibold hover:text-teal-600 cursor-pointer group-hover:scale-105 duration-200 z-[2]">Contact</span>
                                     <span className="w-[300px] md:w-full border-b-[5px] rounded-b-md border-teal-600 scale-0 group-hover:scale-110 duration-200">
                                     </span>
-                                </li>
-                            </Link>
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                     <div className="flex flex-row items-center">
@@ -43,8 +45,8 @@ const Navbar = () => {
                             <li className="cursor-pointer hover:text-purple-600 hover:scale-105 duration-200"><a href="#skills"><i className="fa-solid fa-microchip"></i></a></li>
                             <li className="cursor-pointer hover:text-teal-600 hover:scale-105 duration-200"><a href="#project"><i className="fa-solid fa-briefcase"></i></a></li>
                         </ul>
-                        <div onClick={()=>setOpen(!open)} className="mr-10 text-3xl md:hidden cursor-pointer">
-                            <i name={open ? 'close':'menu'} className={`${open ? 'fa-solid fa-xmark':'fa-solid fa-bars-staggered'}`}></i>
+                        <div onClick={() => setOpen(!open)} className="mr-10 text-3xl md:hidden cursor-pointer">
+                            <i name={open ? 'close' : 'menu'} className={`${open ? 'fa-solid fa-xmark' : 'fa-solid fa-bars-staggered'}`}></i>
                         </div>
                     </div>
                 </div>
